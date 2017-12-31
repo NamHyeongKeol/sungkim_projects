da4e1130f99d34e56f283a494b22731b646d13c1
NamHyeongKeol <grey_pastor@naver.com>
2017-12-31 20:48:41 +0900

    define class Getch

--- rl02/rl02.py
+++ rl02/rl02.py
@@ -2,6 +2,33 @@ import gym
 env = gym.make("FrozenLake-v0")
 observation = env.reset()
 
+
+class _Getch:
+    def __call__(self):
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(3)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+inkey = _Getch()
+
+LEFT = 0
+DOWN = 1
+RIGHT = 2
+UP = 3
+
+arrow_keys = {
+        '\x1b[A': UP,
+        '\x1b[B': DOWN,
+        '\x1b[C': RIGHT,
+        '\x1b[D': LEFT,
+        }
+
+
 for _ in range(1000):
   env.render()
   action = env.action_space.sample()
